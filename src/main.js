04b964e7335d91778f4383a0e510f880fa9bb37e
Ratibor <klov29@mail.ru>
2020-04-23 09:31:02 +0700

    разделяй и властвуй (Часть 2) v1.0

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import TripFilters from "./components/filters.js";
 import TripSort from "./components/sorting.js";
 import NoPoins from "./components/no-poins.js";
 import {getRandomIntegerNumber} from "./../src/utils/common.js";
-import {render, RenderPosition} from "./../src/utils/render.js";
 import TripController from "./../src/controllers/trip.js";
 
 const MIN_DAY = 1;
@@ -18,7 +17,11 @@ const siteTripEvents = document.querySelector(`.trip-events`);
 const siteTripDays = document.createElement(`ul`);
 const siteTripDaysItem = document.createElement(`li`);
 const siteTripEventsList = document.createElement(`ul`);
-const tripController = new TripController(siteTripEvents, siteTripDays, siteTripMain);
+const tripFilters = new TripFilters();
+const tripControls = new TripControls();
+const tripSort = new TripSort();
+const noPoins = new NoPoins();
+
 
 siteTripDays.classList.add(`trip-days`);
 siteTripDaysItem.classList.add(`trip-days__item`);
@@ -28,13 +31,6 @@ siteTripEvents.appendChild(siteTripDays);
 siteTripDays.appendChild(siteTripDaysItem);
 siteTripDaysItem.appendChild(siteTripEventsList);
 
-render(siteTripControls, new TripFilters(), RenderPosition.AFTERBEGIN);
-render(siteTripControls, new TripControls(), RenderPosition.AFTERBEGIN);
-
-if (maxDayEvent > 0) {
-  render(siteTripEvents, new TripSort(), RenderPosition.AFTERBEGIN);
-} else {
-  render(siteTripEvents, new NoPoins(), RenderPosition.AFTERBEGIN);
-}
+const tripController = new TripController(siteTripEvents, siteTripDays, siteTripMain, tripSort, noPoins, siteTripControls, tripFilters, tripControls, [], siteTripEventsList);
 
 tripController.render(maxDayEvent);
